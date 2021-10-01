3e3b16fd904254956ede2668e9255f5b93be4cba
root <root@LAPTOP-BM9S8AHN.localdomain>
2021-10-01 18:45:21 -0300

    Lead create, criando novas leads utilizando forms

--- app/leads/views.py
+++ app/leads/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Lead
+from django.shortcuts import render, redirect
+from .models import Lead, Agent
+from .forms import LeadForm, LeadModelForm
 
 
 def home_page(request):
@@ -12,4 +13,26 @@ def lead_detail(request, pk):
     lead = Lead.objects.get(id=pk)
     context = { 'lead': lead}
     return render(request, 'leads/lead_detail.html', context)
+
+
+def lead_create(request):
+    form = LeadModelForm()
+    if request.method == "POST":
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #first_name = form.cleaned_data['first_name']
+            #last_name = form.cleaned_data['last_name']
+            #age = form.cleaned_data['age']
+            #agent = form.cleaned_data['agent']
+            #Lead.objects.create(
+            #    first_name=first_name,
+            #    last_name=last_name,
+            #    age=age,
+            #    agent=agent
+            #)
+            return redirect('/leads')
+    context = { 'form': form}
+    return render(request, 'leads/lead_create.html', context)
+
     
