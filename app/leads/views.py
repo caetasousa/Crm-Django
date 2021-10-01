0aab15d3a000e4e8e7ca5b5a2680283ee7cef442
root <root@LAPTOP-BM9S8AHN.localdomain>
2021-10-01 16:37:49 -0300

    list view, exibindo todas as leads

--- app/leads/views.py
+++ app/leads/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Lead
 
-# Create your views here.
+
+def home_page(request):
+    leads = Lead.objects.all()
+    context = { 'leads': leads}
+    return render(request, 'leads/lead_list.html', context)
