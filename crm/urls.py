2c5646ce932a0b5db1107092a45e3695072ef68f
root <root@LAPTOP-BM9S8AHN.localdomain>
2021-10-05 20:31:41 -0300

    Home, adicionando home page

--- crm/urls.py
+++ crm/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from app.leads.views import landing_page
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page, name='landing-page'),
     path('leads/',  include('app.leads.urls', namespace="leads"))
 ]
